2482970e5eba7e5d0e8f15a7c277cd9b21c8ad5e
Aiden <aidenireland2004@gmail.com>
2025-04-08 20:59:43 -0400

    added footer and header components

---
+++ ReactFrontEnd/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+const Footer = () => {
+    return (
+      <footer>
+          <h5>Group 6 Members</h5>
+        <h6>Aiden Ireland</h6>
+        <h6>Voltaire Rono</h6>
+        <h6>Ayushi Modi</h6>
+        <h6>UtsavCAN</h6>
+        <br/>
+      </footer>
+    );
+  };
+  
+  export default Footer;
