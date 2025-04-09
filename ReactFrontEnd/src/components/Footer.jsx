159eb433456e99e0a6f5d3a67daca94e7104b821
Aiden <aidenireland2004@gmail.com>
2025-04-08 21:05:10 -0400

    added Utsav name

--- ReactFrontEnd/src/components/Footer.jsx
+++ ReactFrontEnd/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <h6>Aiden Ireland</h6>
         <h6>Voltaire Rono</h6>
         <h6>Ayushi Modi</h6>
-        <h6>UtsavCAN</h6>
+        <h6>Utsav Mistry</h6>
         <br/>
       </footer>
     );
